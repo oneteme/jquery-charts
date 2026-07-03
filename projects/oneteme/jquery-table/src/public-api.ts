0ff13ab9d79c32b7ed777b1a8f777b074a410ba9
Dorian <dorianpernot2@gmail.com>
2026-07-03 15:01:41 +0200

    last

--- projects/oneteme/jquery-table/src/public-api.ts
+++ projects/oneteme/jquery-table/src/public-api.ts
@@ -6,9 +6,6 @@ export * from './lib/jquery-table.model';
 export * from './lib/jqt-i18n.token';
 export * from './lib/component/table.component';
 export * from './lib/component/organizer-button/organizer-button.component';
-// SlicePanel and its models are now hosted in jquery-organizer; re-exported here for backward compat
 export { SliceConfig, SliceColumnDef, SliceCategory, SlicePanelComponent } from '@oneteme/jquery-organizer';
-// OrganizerButtonComponent is now wrapped by ViewButtonComponent
-// Re-exported for backward compatibility (prefer ViewButtonComponent for new code)
 export { OrganizerButtonComponent, OrganizerConfig, OrganizerButtonEvent, OrganizerState } from '@oneteme/jquery-organizer';
 export * from './lib/directive/jqt-cell-def.directive';
